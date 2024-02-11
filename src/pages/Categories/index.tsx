60d2ea20ff6d221f88978d6f9ce02a1cb3d809f9
Ubirajara Neves <biraneves@outlook.com>
2024-02-11 14:10:29 -0300

    Página Categorias e rotas

---
+++ src/pages/Categories/index.tsx
@@ -0,0 +1,103 @@
+import ProductsList from '../../components/ProductsList';
+import Game from '../../models/Game';
+import resident from '../../assets/images/resident.png';
+import diablo from '../../assets/images/diablo.png';
+import starWars from '../../assets/images/star_wars.png';
+import zelda from '../../assets/images/zelda.png';
+
+const promocoes: Game[] = [
+  {
+    id: 1,
+    category: 'Ação',
+    description:
+      'Resident Evil 4, conhecido no Japão como Biohazard 4, é um jogo eletrônico de survival horror...',
+    title: 'Resident Evil 4',
+    system: 'Windows',
+    infos: ['10%', 'R$ 250,00'],
+    image: resident,
+  },
+  {
+    id: 2,
+    category: 'Ação',
+    description:
+      'Resident Evil 4, conhecido no Japão como Biohazard 4, é um jogo eletrônico de survival horror...',
+    title: 'Resident Evil 4',
+    system: 'PS5',
+    infos: ['5%', 'R$ 290,00'],
+    image: diablo,
+  },
+  {
+    id: 3,
+    category: 'Ação',
+    description:
+      'Resident Evil 4, conhecido no Japão como Biohazard 4, é um jogo eletrônico de survival horror...',
+    title: 'Resident Evil 4',
+    system: 'Windows',
+    infos: ['10%', 'R$ 250,00'],
+    image: starWars,
+  },
+  {
+    id: 4,
+    category: 'Ação',
+    description:
+      'Resident Evil 4, conhecido no Japão como Biohazard 4, é um jogo eletrônico de survival horror...',
+    title: 'Resident Evil 4',
+    system: 'Windows',
+    infos: ['10%', 'R$ 250,00'],
+    image: zelda,
+  },
+];
+
+const emBreve: Game[] = [
+  {
+    id: 6,
+    category: 'Ação',
+    description:
+      'Resident Evil 4, conhecido no Japão como Biohazard 4, é um jogo eletrônico de survival horror...',
+    title: 'Resident Evil 4',
+    system: 'Windows',
+    infos: ['17/05'],
+    image: resident,
+  },
+  {
+    id: 7,
+    category: 'Ação',
+    description:
+      'Resident Evil 4, conhecido no Japão como Biohazard 4, é um jogo eletrônico de survival horror...',
+    title: 'Resident Evil 4',
+    system: 'PS5',
+    infos: ['17/05'],
+    image: diablo,
+  },
+  {
+    id: 8,
+    category: 'Ação',
+    description:
+      'Resident Evil 4, conhecido no Japão como Biohazard 4, é um jogo eletrônico de survival horror...',
+    title: 'Resident Evil 4',
+    system: 'Windows',
+    infos: ['17/05'],
+    image: starWars,
+  },
+  {
+    id: 9,
+    category: 'Ação',
+    description:
+      'Resident Evil 4, conhecido no Japão como Biohazard 4, é um jogo eletrônico de survival horror...',
+    title: 'Resident Evil 4',
+    system: 'Nintendo Switch',
+    infos: ['17/05'],
+    image: zelda,
+  },
+];
+
+const Categories = () => (
+  <>
+    <ProductsList title="RPG" background="gray" games={promocoes} />
+    <ProductsList title="Ação" background="black" games={emBreve} />
+    <ProductsList title="Aventura" background="gray" games={promocoes} />
+    <ProductsList title="FPS" background="black" games={emBreve} />
+  </>
+);
+
+export default Categories;
---
+++ src/pages/Home/index.tsx
@@ -0,0 +1,103 @@
+import Banner from '../../components/Banner';
+import ProductsList from '../../components/ProductsList';
+import Game from '../../models/Game';
+import resident from '../../assets/images/resident.png';
+import diablo from '../../assets/images/diablo.png';
+import starWars from '../../assets/images/star_wars.png';
+import zelda from '../../assets/images/zelda.png';
+
+const promocoes: Game[] = [
+  {
+    id: 1,
+    category: 'Ação',
+    description:
+      'Resident Evil 4, conhecido no Japão como Biohazard 4, é um jogo eletrônico de survival horror...',
+    title: 'Resident Evil 4',
+    system: 'Windows',
+    infos: ['10%', 'R$ 250,00'],
+    image: resident,
+  },
+  {
+    id: 2,
+    category: 'Ação',
+    description:
+      'Resident Evil 4, conhecido no Japão como Biohazard 4, é um jogo eletrônico de survival horror...',
+    title: 'Resident Evil 4',
+    system: 'PS5',
+    infos: ['5%', 'R$ 290,00'],
+    image: diablo,
+  },
+  {
+    id: 3,
+    category: 'Ação',
+    description:
+      'Resident Evil 4, conhecido no Japão como Biohazard 4, é um jogo eletrônico de survival horror...',
+    title: 'Resident Evil 4',
+    system: 'Windows',
+    infos: ['10%', 'R$ 250,00'],
+    image: starWars,
+  },
+  {
+    id: 4,
+    category: 'Ação',
+    description:
+      'Resident Evil 4, conhecido no Japão como Biohazard 4, é um jogo eletrônico de survival horror...',
+    title: 'Resident Evil 4',
+    system: 'Windows',
+    infos: ['10%', 'R$ 250,00'],
+    image: zelda,
+  },
+];
+
+const emBreve: Game[] = [
+  {
+    id: 6,
+    category: 'Ação',
+    description:
+      'Resident Evil 4, conhecido no Japão como Biohazard 4, é um jogo eletrônico de survival horror...',
+    title: 'Resident Evil 4',
+    system: 'Windows',
+    infos: ['17/05'],
+    image: resident,
+  },
+  {
+    id: 7,
+    category: 'Ação',
+    description:
+      'Resident Evil 4, conhecido no Japão como Biohazard 4, é um jogo eletrônico de survival horror...',
+    title: 'Resident Evil 4',
+    system: 'PS5',
+    infos: ['17/05'],
+    image: diablo,
+  },
+  {
+    id: 8,
+    category: 'Ação',
+    description:
+      'Resident Evil 4, conhecido no Japão como Biohazard 4, é um jogo eletrônico de survival horror...',
+    title: 'Resident Evil 4',
+    system: 'Windows',
+    infos: ['17/05'],
+    image: starWars,
+  },
+  {
+    id: 9,
+    category: 'Ação',
+    description:
+      'Resident Evil 4, conhecido no Japão como Biohazard 4, é um jogo eletrônico de survival horror...',
+    title: 'Resident Evil 4',
+    system: 'Nintendo Switch',
+    infos: ['17/05'],
+    image: zelda,
+  },
+];
+
+const Home = () => (
+  <>
+    <Banner />
+    <ProductsList title="Promoções" background="gray" games={promocoes} />
+    <ProductsList title="Em breve" background="black" games={emBreve} />
+  </>
+);
+
+export default Home;
